c4f10019365943ee5c3590abb8ec8456903ed8cb
NORMAN Salem <normansalem4@gmail.com>
2025-03-05 11:09:02 +0100

    modification de welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,21 +28,22 @@
                             href="{{ url('/dashboard') }}"
                             class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal"
                         >
-                            Dashboard
+                            Dashboard Affiliation Project
                         </a>
                     @else
                         <a
                             href="{{ route('login') }}"
                             class="inline-block px-5 py-1.5 text-[#1b1b18] border border-transparent hover:border-[#19140035] rounded-sm text-sm leading-normal"
                         >
-                            Log in
+                            Se connecter
                         </a>
 
                         @if (Route::has('register'))
                             <a
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal">
-                                Register
+                                S'inscrire
+                                
                             </a>
                         @endif
                     @endauth
