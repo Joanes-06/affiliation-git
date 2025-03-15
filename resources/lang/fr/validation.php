d2cae37797e17c7b43616ea60c71d290e9a55e19
NORMAN Salem <normansalem4@gmail.com>
2025-03-15 18:19:22 +0100

    modif

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => 'Le champ :attribute est requis.',
+    'email' => 'L\'adresse e-mail :attribute est invalide.',
+    'min' => [
+        'string' => 'Le champ :attribute doit comporter au moins :min caractères.',
+    ],
+    'confirmed' => 'La confirmation du mot de passe ne correspond pas.',
+    // Ajoute d'autres traductions ici selon ton besoin
+];
